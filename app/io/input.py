0bd5eac54894026a7ff15523642850801de2a20f
Tetiana Shapoval <t.shapoval@ukma.edu.ua>
2025-03-31 23:28:21 +0300

    input and output functions added

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+def input_console():
+    pass
+
+def fileread_python():
+    pass
+
+def fileread_pandas():
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_console():
+    pass
+
+def filewrite_python():
+    pass
+
